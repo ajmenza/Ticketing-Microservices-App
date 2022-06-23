6e3ffe6d683e055865f0d7102c69f01ea98a47fa
ajmenza <ajm043@bucknell.edu>
2022-06-23 10:14:58 -0400

    completed authentication microservice

---
+++ auth/src/errors/custom-error.ts
@@ -0,0 +1,12 @@
+// OPTION #2 (better because we can use instance of with it while we can't do something similar with an interface)
+export abstract class CustomError extends Error {
+  abstract statusCode: number;
+
+  constructor(message: string) {
+    super(message);
+
+    Object.setPrototypeOf(this, CustomError.prototype);
+  }
+
+  abstract serializeErrors(): { message: string; field?: string }[];
+}
